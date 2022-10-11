a6bea6ab127c452a11ebed66d5b5de334dc975de
Natalia Chaika <nataliia.chaika@ucu.edu.ua>
2022-10-11 10:29:16 +0300

    final test check. character factory defined

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -1,13 +1,19 @@
 package lotr;
-
 import lombok.SneakyThrows;
-
+import org.reflections.Reflections;
 import java.util.Random;
-
+import java.util.Set;
+import static org.reflections.scanners.Scanners.SubTypes;
+//class CharacterFactory
+//   - methods: Character createCharacter() {
+//           returns random instance of any existing character
+//           }
 public class CharacterFactory {
-//    @SneakyThrows
-//    public Character createCharacter() {
-//        Character[] characters = {new Hobbit(), new King(), new Knight()};
-//        return characters[new Random().nextInt(characters.length)].getDeclaredConstructor().
-//    }
+    @SneakyThrows
+    public Character createCharacter() {
+        Reflections reflections = new Reflections("lotr");
+        Set<Class<?>> subTypes = reflections.get(SubTypes.of(Character.class).asClass());
+        Class cls = (Class) subTypes.toArray()[new Random().nextInt(subTypes.size())];
+        return (Character) cls.getDeclaredConstructor().newInstance();
+    }
 }
--- src/main/java/lotr/Knight.java
+++ src/main/java/lotr/Knight.java
@@ -13,8 +13,5 @@ public class Knight extends Character {
     //class Knight
 //   - attributes: int power=from 2 to 12, int hp=from 2 to 12
 //           - methods: void kick(Character c) { like lotr.King }
-//class CharacterFactory
-//   - methods: Character createCharacter() {
-//           returns random instance of any existing character
-//           }
+
 }
